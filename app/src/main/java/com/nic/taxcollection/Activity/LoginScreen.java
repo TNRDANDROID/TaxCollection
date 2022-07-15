7179a86edb8a09aa6a7c542169c1034c6c506625
AchanthiSundar <tnrdmobile@gmail.com>
2022-07-15 15:54:45 +0530

    initial commit

---
+++ app/src/main/java/com/nic/taxcollection/Activity/LoginScreen.java
@@ -0,0 +1,16 @@
+package com.nic.taxcollection.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nic.taxcollection.R;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
---
+++ app/src/main/java/com/nic/taxcollection/Activity/SplashScreen.java
@@ -0,0 +1,48 @@
+package com.nic.taxcollection.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.nic.taxcollection.R;
+
+public class SplashScreen extends AppCompatActivity implements View.OnClickListener {
+
+    Button sign_in,sign_up;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        initializeUI();
+    }
+
+    private void initializeUI() {
+        sign_in = findViewById(R.id.sign_in);
+        sign_up = findViewById(R.id.sign_up);
+
+        sign_in.setOnClickListener(this::onClick);
+        sign_up.setOnClickListener(this::onClick);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.sign_in:
+                gotoLoginScreen();
+                break;
+            case R.id.sign_up:
+                break;
+        }
+    }
+
+    private void gotoLoginScreen() {
+        Intent intent = new Intent(SplashScreen.this,LoginScreen.class);
+        intent.putExtra("Home","No");
+        startActivity(intent);
+    }
+}
